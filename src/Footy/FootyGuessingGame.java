aac766f1e68bd6b3f63b0f5a6f07800828c06e84
unknown <s.ure@Lab054-N205-10.du054.edu.epsb.ca>
2012-03-20 11:05:21 -0600

    added the first question

--- src/Footy/FootyGuessingGame.java
+++ src/Footy/FootyGuessingGame.java
@@ -1,11 +1,19 @@
 package Footy;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class FootyGuessingGame {
-
+    
+    int guessCount = 0;
+    String answer;
+    boolean gameOn = false;
+    
     public static void main(String[] args) {
-     
+        int guessCount = 0;
+        String answer;
+        
         ArrayList teams = new ArrayList();
         teams.add("Arsenal");
         teams.add("Tottenham");
@@ -26,20 +34,55 @@ public class FootyGuessingGame {
         teams.add("Ajax");
         teams.add("Benfica");
         Iterator checkTeams = teams.iterator();
-
+        Scanner stringScan = new Scanner(System.in);
+        System.out.println("In a moment, I will give you a list of soccer teams \n"
+                + "to choose from, and I will guess which one you picked based \n"
+                + "on a series of questions that I ask you. There is one \n"
+                + "condition though. You have to answer either yes or no. Ready?");
+        System.out.println("Pick a team:");
         while (checkTeams.hasNext()) {
-
+            
             System.out.println(checkTeams.next());
-
+            
+        }
+        System.out.println("Do you have your team?");
+        answer = stringScan.nextLine();
+        if (answer.contains("yes") || answer.startsWith("y")) {
+            boolean gameOn = true;
+            System.out.println("Sweet!\n"
+                    + "Has your team won the modern installation of the Champions League (1993-present)?");
+            answer = stringScan.nextLine();
+            if (answer.contains("yes") || answer.startsWith("y")) {
+                
+                teams.remove("Arsenal");
+                teams.remove("Manchester City");
+                teams.remove("Tottenham");
+                teams.remove("Lyon");
+                teams.remove("Benfica");
+                teams.remove("Bayer Leverkusen");
+                teams.remove("Chelsea");
+                
+            } else {
+                teams.remove("Liverpool");
+                teams.remove("Manchester United");
+                teams.remove("Barcelona");
+                teams.remove("Real Madrid");
+                teams.remove("Marseille");
+                teams.remove("AC Milan");
+                teams.remove("Inter");
+                teams.remove("Juventus");
+                teams.remove("Bayern Munich");
+                teams.remove("Borussia Dortmund");
+                teams.remove("Ajax");
+            }
         }
         
-        teams.remove(2);
         checkTeams = teams.iterator();
         
-        while (checkTeams.hasNext()){
+        while (checkTeams.hasNext()) {
+            
             System.out.println(checkTeams.next());
+            
         }
-        
-        
     }
 }
