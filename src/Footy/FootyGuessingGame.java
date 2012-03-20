e214b105e8370937deffb4e95dbc3266f91aad27
Stuart Ure <stuart.ure23@gmail.com>
2012-03-19 22:47:58 -0600

    Added more teams

--- src/Footy/FootyGuessingGame.java
+++ src/Footy/FootyGuessingGame.java
@@ -12,6 +12,18 @@ public class FootyGuessingGame {
      teams.add("Liverpool");
      teams.add("Manchester United");
      teams.add("Manchester City");
+     teams.add("AC Milan");
+     teams.add("Inter");
+     teams.add("Juventus");
+     teams.add("Real Madrid");
+     teams.add("Barcelona");
+     teams.add("Lyon");
+     teams.add("Marseille");
+     teams.add("Bayern Munich");
+     teams.add("Borussia Dortmund");
+     teams.add("Bayer Leverkusen");
+     teams.add("Ajax");
+     teams.add("Benfica");
      Iterator checkTeams = teams.iterator();
      
      while (checkTeams.hasNext()){
